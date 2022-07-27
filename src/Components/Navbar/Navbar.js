f2b610ae4ef7a7ef0d4fdbc4bebd4fa192b3d7f5
raselcsedev <raselcse.dev@gmail.com>
2022-07-27 18:46:38 +0600

    home page and navbar designed

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Link,useMatch,useResolvedPath} from "react-router-dom";
+  
+import logo from './../../images/globetech logo.png'
+import './Navbar.css'
+
+const Navbar = () => {
+    function CustomLink({ children, to, ...props }) {
+        let resolved = useResolvedPath(to);
+        let match = useMatch({ path: resolved.pathname, end: true });
+        return (
+          <div >
+            <Link
+              style={{ color: match ? "#CBF231" : "#2EBBE5" }}
+              to={to}
+              {...props}
+            >
+              {children}
+            </Link>
+         
+          </div>
+        );
+      }
+      return (
+          <div className='nav'>
+             <nav className='navbar'>
+              <div className='logo'>
+                  <img src={logo} alt=""  />
+              </div>
+              <div>
+                  <ul>
+                      <li><CustomLink to="/">Home</CustomLink></li>
+                      <li><CustomLink to="service">Service</CustomLink></li>
+                      <li><CustomLink to="login">Login</CustomLink></li>
+                      
+                  </ul>
+              </div>
+             </nav>
+              
+          </div>
+      );
+  };
+
+export default Navbar;
