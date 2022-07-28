37eadb47532d3e3183218a79fb04f4c601caf1ba
raselcsedev <raselcse.dev@gmail.com>
2022-07-28 09:47:01 +0600

    login page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
       </Routes>
     </div>
   );
